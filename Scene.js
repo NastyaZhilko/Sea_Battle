1201043bb6eb3e457e10fb8990bf888bb5b48697
NastyaZhilko <nasten4ik2010@gmail.com>
2021-12-09 23:07:08 +0300

    first commit

---
+++ Scene.js
@@ -0,0 +1,17 @@
+
+class Scene {
+    name = null;
+    app = null;
+
+    constructor(name, app) {
+        Object.assign(this, { name, app });
+    }
+
+    init() {}
+
+    start() {}
+
+    update() {}
+
+    stop() {}
+}
---
+++ script.js
@@ -0,0 +1,9 @@
+const app = new Application({
+    preparation: PreparationScene,
+    computer: ComputerScene
+});
+
+app.start("preparation");
+
+
+
